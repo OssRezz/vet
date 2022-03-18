f3228db9f9481e642d77648721f5ea3de050dad4
James Florez <OssRezz.13@gmail.com>
2022-03-18 13:32:27 -0500

    add

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'login/singin',  // exclude all URLs wit payment/ prefix
+        'login/singout' // exclude exact URL
     ];
 }
